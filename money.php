743c3c03365133e94617d346bfb0a4f88fb7c9e3
Md-1122 <91255988+Md-1122@users.noreply.github.com>
2023-08-16 04:30:26 -0700

    code updated

---
+++ money.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>payment</title>
+    <link rel="Website Icon" type="png" href="images/2.png">
+    <link rel="stylesheet" type="text/css" href="moneystyle.css">
+</head>
+
+<body bgcolor="#1D4B8F">
+    <div class="both">
+        <div class="x">
+            <p>
+            <h2>Complete Payment</h2>
+            Enter your credit or debit carddetais below<br>
+            <input name="credit-number" class="cc-number" type="tel" pattern="\d*" maxlength="16" placeholder="Card Number" required>
+            <input name="credit-expires" class="cc-expires" type="tel" pattern="\d*" maxlength="7" placeholder="MM / YY" required>
+            <input name="credit-cvc" class="cc-cvc" type="tel" pattern="\d*" maxlength="4" placeholder="CVC" required><br>
+            <br>
+            <input type="submit" class="form-submit" value="Confirm Payment">
+
+            </p>
+            <div class="">
+                <h3>order summary</h3>
+            </div>
+        </div>
+       
+    </div>
+
+
+
+</body>
+
+</html>
